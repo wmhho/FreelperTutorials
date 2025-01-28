9743892a6d809fc42b47e2ab8f50e67763bd92a7
wmhho <116261192+wmhho@users.noreply.github.com>
2025-01-28 23:06:12 +0000

    Create EquipmentStats.cs

---
+++ EquipmentStats.cs
@@ -0,0 +1,8 @@
+// 3
+
+[CreateAssetMenu(fileName = 'insertfilehere', menuName = 'insertmenuhere')]
+public class EquipmentStats: ScriptableObject
+{
+  // OPTIONAL TODO: Do a scriptable object for Equipments too!
+  // If you are stuck, follow the UnitStats.cs scriptable object
+}
